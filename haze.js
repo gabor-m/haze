32becb19b7236cea74a4ec23e978f5239f27beba
Menyhert Gabor <gazbor@gmail.com>
2020-01-30 23:21:14 +0100

    error handling

--- haze.js
+++ haze.js
@@ -5,6 +5,23 @@ var codegen = require("./codegen.js");
 
 var source_code = process.argv[2] || "";
 
-codegen(source_code).eval();
+try {
+    codegen(source_code).eval();
+} catch (e) {
+    try {
+        if (typeof e === "object" && e.type) {
+            console.log("\u0000Error: " + runtime.to_string().value);
+        } else if (typeof e === "object" && e.message) {
+            console.log("\u0000Error: " + String(e.message));
+        } else if (typeof e === "string") {
+            console.log("\u0000Error: " + e);
+        } else {
+            console.log("\u0000Unknow error");
+        }
+    } catch (e) {
+        console.log("\u0000Unknow error");
+    }
+}
 
 // process.stdout.write('\u0000');
+
