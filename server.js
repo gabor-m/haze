8d828258663d8e7a41e03c8e22c7d04001bc1541
Menyhert Gabor <gazbor@gmail.com>
2020-01-31 10:25:05 +0100

    save

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+var fs = require('fs');
 var http = require('http');
 var child_process = require('child_process');
+var crypto = require('crypto')
 
 var TIMEOUT = 10; // sec
+
+function sha1(str) {
+    var shasum = crypto.createHash('sha1');
+    shasum.update(str);
+    return shasum.digest('hex');
+}
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'*' });
@@ -11,24 +19,42 @@ http.createServer(function (req, res) {
             body += data.toString();
         });
         req.on('end', function () {
-            var child = child_process.spawn("node", ["haze.js", body.replace(/[\r\n]/g, '\n')], {
-                timeout: 1 // ms
-            });
-            // console.log(body);
-            child.stdout.on('data', function (data) {
-                data = data.toString();
-                res.write(data);
-            });
-            child.stdout.on('end', function (data) {
-                res.end();
-            });
-            setTimeout(function () {      
-                child.kill();
-                if (!res.finished) {
-                    res.write("timeout");
-                    res.end();
+            if (body.indexOf("%save") === 0) {
+                body = body.substr(5);
+                var filename = sha1(body);
+                var path = "./saved/" + filename + ".hz";
+                if (!fs.existsSync(path)) {
+                    fs.writeFileSync(path, body, "utf8");
                 }
-            }, TIMEOUT * 1000);
+                res.write(filename);
+                res.end();
+            } else if (body.indexOf("%get") === 0) {
+                body = body.substr(4);
+                var path = "./saved/" + body + ".hz";
+                if (body.length !== 40 || !fs.existsSync(path)) {
+                    res.write('');
+                } else {
+                    res.write(fs.readFileSync(path, 'utf8'));
+                }
+                res.end();
+            } else {
+                var child = child_process.spawn("node", ["haze.js", body.replace(/[\r\n]/g, '\n')]);
+                // console.log(body);
+                child.stdout.on('data', function (data) {
+                    data = data.toString();
+                    res.write(data);
+                });
+                child.stdout.on('end', function (data) {
+                    res.end();
+                });
+                setTimeout(function () {      
+                    child.kill();
+                    if (!res.finished) {
+                        res.write("timeout");
+                        res.end();
+                    }
+                }, TIMEOUT * 1000);
+            }
         });
         
     } else {
